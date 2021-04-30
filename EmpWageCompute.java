8569d2d50c04fcb205901d0de0518adad95bd84e
shripadshukla <shripadshukla46@gmail.com>
2021-05-01 00:00:24 +0530

    UC5: Wage computation for a month added successfully

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -5,28 +5,21 @@ public class EmpWageCompute {
 
 		// TODO Auto-generated method stub
 		
-		int Employee = 1;
-		int Full_time_Wage = 20;
-		int Full_time_Hour = 8;
-		int Part_time_Hour = 4;
-		
-		// Total working days for a month
-		int Working_days = 20;
-		
-		switch (Employee) {
-		
-			case 1:
-				System.out.println("Employee is Full Time Present");
+				int Full_time_employee = 1;
+				int Part_time_employee = 2;
+				int Full_time_Wage = 20;
+				int Full_time_Hour = 8;
+				int Part_time_Hour = 4;
+				
+				// Total working days for a month
+				int Working_days = 20;
 				
 				double Emp_Wage1 = (Working_days * Full_time_Wage * Full_time_Hour);
-				System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
-			
-			default:
-				System.out.println("Employee is Part Time Present");
 				
 				double Emp_Wage2 = (Working_days * Full_time_Wage * Part_time_Hour);
+				
+				System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
 				System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2);
-			}
 
 		}
 }
