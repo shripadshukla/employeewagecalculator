fa485c419e463a550abc85e221dd8798e760db21
shripadshukla <shripadshukla46@gmail.com>
2021-04-30 23:47:51 +0530

    UC3: PartTime Wage added successfully

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -3,12 +3,20 @@ public class EmpWageCompute {
 
 	public static void main(String[] args) {
 		
-		int Wage_Per_Hour = 20;
-		int Full_day_Hour = 8;
+		int full_time_Employee = 1;
+		int part_time_Employee = 2;
+		int wage_Per_Hour = 20;
+		int full_day_Hour = 8;
+		int part_time_Hour =4;
+
+			
+			double full_time_EmpdailyWage = (wage_Per_Hour * full_day_Hour);
 		
-		double EmpdailyWage = (Wage_Per_Hour * Full_day_Hour);
-		
-		System.out.println("Employee daily wage is = "+EmpdailyWage);
+			double part_time_EmpdailyWage = (wage_Per_Hour * part_time_Hour);
+			
+		System.out.println("Full Time Employee daily wage is = "+full_time_EmpdailyWage);
+	
+		System.out.println("Part Time Employee daily wage is = "+ part_time_EmpdailyWage);
 			}
 
 		}
