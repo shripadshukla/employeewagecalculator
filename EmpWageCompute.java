de89089cad12ac330b189787b9359f9e514ae077
shripadshukla <shripadshukla46@gmail.com>
2021-04-30 23:30:54 +0530

    Added: Added Attendance Tracker

---
+++ EmpWageCompute.java
@@ -0,0 +1,20 @@
+
+public class EmpWageCompute {
+
+	public static void main(String[] args) {
+		
+				int IS_PRESENT = 1;
+				
+				double empCheck = Math.floor(Math.random() * 10) % 2;
+				
+				if (empCheck == IS_PRESENT)
+					
+					System.out.println("Employee is present");
+				
+				else
+
+					System.out.println("Emppployee is absent");
+			}
+
+		}
+
